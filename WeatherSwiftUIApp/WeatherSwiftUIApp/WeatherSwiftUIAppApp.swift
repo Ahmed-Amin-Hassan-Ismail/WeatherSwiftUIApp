8e5aadf87cd21716f5673e91b272a9b839bef735
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-12-19 17:48:16 +0200

    initial app and create xcode project

---
+++ WeatherSwiftUIApp/WeatherSwiftUIApp/WeatherSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIAppApp.swift
+//  WeatherSwiftUIApp
+//
+//  Created by Ahmed Amin on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
