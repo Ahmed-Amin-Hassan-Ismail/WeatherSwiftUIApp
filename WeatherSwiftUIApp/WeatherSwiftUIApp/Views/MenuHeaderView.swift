b9570f74579fb45c9e00edc1b974fb90d031a84d
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-12-20 16:08:17 +0200

    create menu header view

---
+++ WeatherSwiftUIApp/WeatherSwiftUIApp/Views/MenuHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  MenuHeaderView.swift
+//  WeatherSwiftUIApp
+//
+//  Created by Ahmed Amin on 20/12/2022.
+//
+
+import SwiftUI
+
+struct MenuHeaderView: View {
+    @ObservedObject var cityViewModel: CityViewModel
+    @State private var cityName: String = "San Francisco"
+    
+    var body: some View {
+        HStack {
+            TextField("", text: $cityName)
+                .padding(.leading, 20)
+            
+            Button {
+                cityViewModel.city = cityName
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                    
+                    Image(systemName: "location.fill")
+                }
+            }
+            .frame(width: 50, height: 50)
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(ZStack(alignment: .leading, content: {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.black)
+                .padding(.leading, 10)
+            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.blue.opacity(0.5))
+        }))
+    }
+}
+
+struct MenuHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuHeaderView(cityViewModel: CityViewModel())
+    }
+}
